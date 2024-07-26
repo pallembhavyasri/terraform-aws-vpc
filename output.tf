d1cf50d30587296fcc183b53d34acf5ca8b7412d
Bhavya Sri P <pallembhavya1@gmail.com>
2024-07-26 15:52:30 +0530

    terraform aws vpc

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "avalibility_zones"{
-    value = data.aws_availability_zones.available.names
-}
+# output "avalibility_zones"{
+#     value = data.aws_availability_zones.available.names
+# }
 
 output "vpc_id"{
     value = aws_vpc.main.id
